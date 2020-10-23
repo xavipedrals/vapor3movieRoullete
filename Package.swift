24b025302ef46b083781bbf42117c3989360146a
Xavi Pedrals <xavi.pedrals@gmail.com>
2020-10-23 12:41:22 +0200

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         .package(url: "https://github.com/zmeyc/telegram-bot-swift.git", from: "2.0.0"),
         .package(url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.1"),
-        .package(url: "https://github.com/givip/Telegrammer.git", from: "0.4.0")
+        .package(url: "https://github.com/givip/Telegrammer.git", .exact("0.5.0"))
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "TelegramBotSDK", "Jobs", "Telegrammer"]),
